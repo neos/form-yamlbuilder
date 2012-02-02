a0b6aacdb6b5ce73d1011d16964fa2e9c733efdd
Sebastian Kurfuerst <sebastian@typo3.org>
2012-02-02 15:54:47 +0100

    Fig Drag/Drop in list

--- Resources/Public/Library/SlickGrid/slick.grid.js
+++ Resources/Public/Library/SlickGrid/slick.grid.js
@@ -1253,7 +1253,6 @@ if (typeof Slick === "undefined") {
     }
 
     function resizeCanvas() {
-		console.log("Resize canvas");
       if (options.autoHeight) {
         viewportH = options.rowHeight * (getDataLength() + (options.enableAddRow ? 1 : 0) + (options.leaveSpaceForNewRows ? numVisibleRows - 1 : 0));
       } else {
