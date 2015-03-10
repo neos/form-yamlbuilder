7b7ea57227749af6d24a875172067884804d54e8
Bastian Waidelich <bastian@typo3.org>
2015-03-10 18:00:13 +0100

    [TASK] Apply recent core migrations
    Marks recent core migrations applied and adds
    "escapeOutput" property to the ``JsonConfigurationViewHelper``
    to ensure backwards-compatibility
    
    Change-Id: Ia3188be1bc414936c0914b5231a67e9b68c19b49
    Migration: TYPO3.FLOW3-201201261636
    Migration: TYPO3.Fluid-201205031303
    Migration: TYPO3.FLOW3-201205292145
    Migration: TYPO3.FLOW3-201206271128
    Migration: TYPO3.Flow-201211151101
    Migration: TYPO3.Flow-201212051340
    Migration: TYPO3.Flow-201310031523
    Migration: TYPO3.Flow-201405111147
    Migration: TYPO3.Fluid-20141113120800
    Migration: TYPO3.Flow-20141113121400
    Migration: TYPO3.Fluid-20141121091700
    Migration: TYPO3.Fluid-20150214130800

--- Classes/TYPO3/FormBuilder/ViewHelpers/JsonConfigurationViewHelper.php
+++ Classes/TYPO3/FormBuilder/ViewHelpers/JsonConfigurationViewHelper.php
@@ -18,6 +18,12 @@ use TYPO3\Flow\Annotations as Flow;
  */
 class JsonConfigurationViewHelper extends \TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper {
 
+
+	/**
+	 * @var boolean
+	 */
+	protected $escapeOutput = FALSE;
+
 	/**
 	 * @Flow\Inject
 	 * @var \TYPO3\Flow\Resource\Publishing\ResourcePublisher
